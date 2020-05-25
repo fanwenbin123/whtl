50211caac16a1cd84d52fbc652e9fa1b748ee5c0
fanwenbin <476767207@qq.com>
2020-05-25 23:58:09 +0800

    构建项目手脚架

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import '@/assets/scss/index.scss'
 import '@/utils/vant'
-import vConsole from './vconsole.js'
+//import vConsole from './vconsole.js'
+
 
 import FastClick from 'fastclick'
 FastClick.attach(document.body)
